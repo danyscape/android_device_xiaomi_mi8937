ad3b7c82cff8f9c372af27e5c7ca151005f2b988
dany <adamzulkey@gmail.com>
2021-12-29 17:08:25 +0800

    mi8937: switch to raven fp

--- aosp_mi8937.mk
+++ aosp_mi8937.mk
@@ -30,7 +30,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="ugg-user 7.1.2 N2G47H V11.0.2.0.NDKMIXM release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.017 7955197 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/ugg/ugg:7.1.2/N2G47H/V11.0.2.0.NDKMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.017/7955197:user/release-keys"
